06656e64b41dee676a9659d3f2cd2b738518fcbc
EternalTempter <FrOsTiK12345678998@gmail.com>
2022-09-30 00:49:32 +0200

    asdsadfasdfasf

--- app.js
+++ app.js
@@ -1,18 +1,14 @@
 const express = require("express");
+const path = require("path");   
+
+const port = process.env.PORT || 8080;
+
 const app = express();
+app.use(express.static(__dirname))
+app.use(express.static(path.resolve(__dirname, 'build')))
 
-const path = require("path");
-
-const port = process.env.PORT || 4000;
-
-if(process.env.NODE_ENV === 'production') {
-    app.use(express.static('build'));
-    app.get('*', (req, res) => {
-        req.sendFile(path.resolve(__dirname, 'build', 'index.html'));
-    })
-}
-
-app.listen(port, (err) => {
-    if(err) return console.log(err);
-    console.log('Server running on port: ', port);
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, 'build', 'index.html'))
 })
+
+app.listen(port);
